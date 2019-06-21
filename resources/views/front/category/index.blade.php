90780b3c125ec4fba164cb13e20fd3ed9b6db7db
Thucle <ba.thuc@primelabo.com.vn>
2019-06-21 17:31:35 +0700

    update content

--- resources/views/front/category/index.blade.php
+++ resources/views/front/category/index.blade.php
@@ -2,11 +2,13 @@
 
 @section('content')
     <div class="pt-5"></div>
-    @if(!empty($postList))
+    @if($postList->count())
         @foreach ($postList as $post)
             <a class="post-title" href="{{ route('front.route_name', $post->route) }}">{{ $post->title }}</a> <br>
         @endforeach
         {{ $postList->links() }}
+    @else
+        <h2 class="text-danger">Content not found</h2>
     @endif
     <style>
         .post-title{
